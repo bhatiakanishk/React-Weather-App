2c5d846bbbcec3042447477d21dbe5a610463b0e
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-11 19:39:58 +0530

    Update App.js

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,12 +1,35 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 
-//const url = `https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=imperial&appid=d694536b035ba20c9b164f10caf2a3a3`
+//const url = `https://api.openweathermap.org/data/2.5/weather?lat=27&lon=77&appid=d694536b035ba20c9b164f10caf2a3a3`
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Mumbai</p>
+          </div>
+          <div className="temp">
+            <h1>86°F</h1>
+          </div>
+          <div className="description">
+            <p>Rains</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>90°F</p>
+          </div>
+          <div className="humidity">
+            <p>85%</p>
+          </div>
+          <div className="wind">
+            10 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
