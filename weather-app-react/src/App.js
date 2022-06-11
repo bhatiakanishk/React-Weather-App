f70f3294ba708e8b60bbb703025713f7940fc610
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-11 19:22:28 +0530

    initial commit

---
+++ weather-app-react/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+//const url = `https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=imperial&appid=d694536b035ba20c9b164f10caf2a3a3`
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
